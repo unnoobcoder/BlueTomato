fa71555be1f7d564cfd9dabc6905f5b80fb40c22
unnoobcoder <sjoerdcode@gmail.com>
2016-09-19 20:16:04 +0200

    fibonacci function added (not functional)

--- src/fibonacci.py
+++ src/fibonacci.py
@@ -1,8 +1,14 @@
-def fibonacci():
-    x = 1
-    while x < 2000:
-        old = x - x
-        x = x + old
+# def fibonacci():
+#    x = 1
+#    while x < 2000:
+#        old = x - x
+#        x = x + old        # Old fibonnaci code
+#
+#        print(x)
+# fibonacci()
 
-        print(x)
-fibonacci()
+
+fib=[0,1]
+for i in range(50):
+    fib.append(fib[-1]+fib[-2])         #easier and smarter code
+print(fib)
