0e346b09f9b463549966819594814bf9efb35906
unnoobcoder <sjoerdcode@gmail.com>
2016-09-19 20:00:03 +0200

    Initial commit

---
+++ src/main.py
@@ -0,0 +1,17 @@
+import math
+import statistics
+print('Hello internet please pick plus, minus, divide or multiply')
+class calculator:
+    def plus(x,y):
+        plus = x + y
+        print(plus)
+    def minus(x,y):
+        minus = x - y
+        print(minus)
+    def divide(x,y):
+        divide = x / y
+        print(divide)
+    def multiply(x,y):
+        multiply = x * y
+        print(multiply)
+calculator.divide(100,10)
